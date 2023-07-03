7da69499d8b2664d235ed9cc31de7b3c649da19c
hwo <hwong23@gmail.com>
2023-07-03 10:34:35 -0500

    cita

--- build/build.sh
+++ build/build.sh
@@ -36,7 +36,7 @@ manubot process \
   --content-directory=content \
   --output-directory=output \
   --cache-directory=ci/cache \
-  --skip-citations \
+  # --skip-citations \
   --log-level=INFO
 
 # Make output directory
